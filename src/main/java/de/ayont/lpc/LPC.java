b980b61871fa7fb89fb5f7b4ebe730d2ef5e8bf0
Ayont <ayontprivat@gmail.com>
2024-05-14 00:25:43 +0200

    Refactor LPC class and update dependencies
    The main LPC class has been refactored to exclude unused imports and better structure methods. Unnecessary event listeners have been removed and commands have now been outsourced to a separate class (LPCCommand). Furthermore, the AsyncChatListener is now being registered to handle chat events, and the dependency on PlaceholderAPI has been eliminated for a cleaner, leaner implementation.

--- src/main/java/de/ayont/lpc/LPC.java
+++ src/main/java/de/ayont/lpc/LPC.java
@@ -1,28 +1,23 @@
 package de.ayont.lpc;
 
-import me.clip.placeholderapi.PlaceholderAPI;
+import de.ayont.lpc.commands.LPCCommand;
+import de.ayont.lpc.listener.AsyncChatListener;
+import io.papermc.paper.event.player.AsyncChatEvent;
 import net.kyori.adventure.text.minimessage.MiniMessage;
-import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import net.luckperms.api.LuckPerms;
-import net.luckperms.api.cacheddata.CachedMetaData;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
-public final class LPC extends JavaPlugin implements Listener {
+public final class LPC extends JavaPlugin {
 
     private LuckPerms luckPerms;
-    private MiniMessage miniMessage;
+    private final MiniMessage miniMessage;
 
     public LPC() {
         this.miniMessage = MiniMessage.builder().build();
@@ -30,55 +25,23 @@ public final class LPC extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // Load an instance of 'LuckPerms' using the services manager.
         this.luckPerms = getServer().getServicesManager().load(LuckPerms.class);
+        registerCommand();
 
         saveDefaultConfig();
-        getServer().getPluginManager().registerEvents(this, this);
+        registerListeners();
     }
 
-    @Override
-    public boolean onCommand(final CommandSender sender, final Command command, final String label, final String[] args) {
-        if (args.length == 1 && "reload".equals(args[0])) {
-            reloadConfig();
+    public void registerCommand() {
+        String commandName = "lpc";
+        LPCCommand lpcCommand = new LPCCommand(this);
 
-            sender.sendMessage("LPC has been reloaded.");
-            return true;
-        }
-
-        return false;
+        this.getCommand(commandName).setExecutor(lpcCommand);
+        this.getCommand(commandName).setTabCompleter(lpcCommand);
     }
 
-    @Override
-    public List<String> onTabComplete(final CommandSender sender, final Command command, final String alias, final String[] args) {
-        if (args.length == 1)
-            return Collections.singletonList("reload");
-
-        return new ArrayList<>();
+    public void registerListeners() {
+        getServer().getPluginManager().registerEvents(new AsyncChatListener(this), this);
     }
 
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void onChat(final AsyncPlayerChatEvent event) {
-        final String message = event.getMessage();
-        final Player player = event.getPlayer();
-
-        final CachedMetaData metaData = this.luckPerms.getPlayerAdapter(Player.class).getMetaData(player);
-        final String group = metaData.getPrimaryGroup();
-
-        String format = getConfig().getString(getConfig().getString("group-formats." + group) != null ? "group-formats." + group : "chat-format")
-                .replace("&", "§")
-                .replace("{prefix}", metaData.getPrefix() != null ? metaData.getPrefix() : "")
-                .replace("{suffix}", metaData.getSuffix() != null ? metaData.getSuffix() : "")
-                .replace("{prefixes}", metaData.getPrefixes().keySet().stream().map(key -> metaData.getPrefixes().get(key)).collect(Collectors.joining()))
-                .replace("{suffixes}", metaData.getSuffixes().keySet().stream().map(key -> metaData.getSuffixes().get(key)).collect(Collectors.joining()))
-                .replace("{world}", player.getWorld().getName())
-                .replace("{name}", player.getName())
-                .replace("{displayname}", player.getDisplayName())
-                .replace("{username-color}", metaData.getMetaValue("username-color") != null ? metaData.getMetaValue("username-color") : "")
-                .replace("{message-color}", metaData.getMetaValue("message-color") != null ? metaData.getMetaValue("message-color") : "");
-
-
-        format = getServer().getPluginManager().isPluginEnabled("PlaceholderAPI") ? PlaceholderAPI.setPlaceholders(player, format) : format;
-
-        event.setFormat(LegacyComponentSerializer.legacySection().serialize(miniMessage.deserialize(format.replace("{message}", message))));    }
 }
