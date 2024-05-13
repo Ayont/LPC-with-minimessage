4d63ff7fde98b58628f71fd835e574f439919213
Ayont <ayontprivat@gmail.com>
2024-05-14 01:18:16 +0200

    Remove unused imports in LPC.java
    This commit strips down the LPC.java file by removing unused import statements. This is done to improve resource efficiency and code maintainability.

--- src/main/java/de/ayont/lpc/LPC.java
+++ src/main/java/de/ayont/lpc/LPC.java
@@ -2,17 +2,10 @@ package de.ayont.lpc;
 
 import de.ayont.lpc.commands.LPCCommand;
 import de.ayont.lpc.listener.AsyncChatListener;
-import io.papermc.paper.event.player.AsyncChatEvent;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.luckperms.api.LuckPerms;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.TabCompleter;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public final class LPC extends JavaPlugin {
 
