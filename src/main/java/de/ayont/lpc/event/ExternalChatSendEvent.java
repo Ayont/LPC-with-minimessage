18a2db689a68f7d7acdcde69873c7c118200083a
Ayont <ayontprivat@gmail.com>
2024-05-14 00:50:56 +0200

    Add `ExternalChatSendEvent` class
    A new class called `ExternalChatSendEvent` has been created in the package de.ayont.lpc.event. This class is an event handler which extends the `Event` class and implements the `Cancellable` interface. It contains methods related to chat messages in the application such as setting message format, retrieving the unique identifier, and handling event cancellation.

---
+++ src/main/java/de/ayont/lpc/event/ExternalChatSendEvent.java
@@ -0,0 +1,87 @@
+package de.ayont.lpc.event;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.UUID;
+
+public class ExternalChatSendEvent extends Event implements Cancellable {
+    private static final HandlerList HANDLERS = new HandlerList();
+    private final UUID uuid;
+    private String name;
+    private String format;
+    private String message;
+    private boolean cancel;
+
+    public ExternalChatSendEvent(UUID uuid, String name, String format, String message) {
+        super(true);
+
+        this.uuid = uuid;
+        this.name = name;
+        this.format = format;
+        this.message = message;
+    }
+
+    @NotNull
+    public static HandlerList getHandlerList() {
+        return HANDLERS;
+    }
+
+    @Nullable
+    public UUID getUUID() {
+        return uuid;
+    }
+
+    @Nullable
+    public Player getPlayer() {
+        return uuid != null ? Bukkit.getPlayer(uuid) : null;
+    }
+
+    @NotNull
+    public String getName() {
+        return name;
+    }
+
+    public void setName(@NotNull String name) {
+        this.name = name;
+    }
+
+    @NotNull
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(@NotNull String format) {
+        this.format = format;
+    }
+
+    @NotNull
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(@NotNull String message) {
+        this.message = message;
+    }
+
+    @NotNull
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLERS;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancel;
+    }
+
+    @Override
+    public void setCancelled(boolean cancel) {
+        this.cancel = cancel;
+    }
+}
