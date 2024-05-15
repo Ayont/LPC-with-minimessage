79358c8efac10528ac3fd9bfc0a9402987fbdb4c
Ayont <ayontprivat@gmail.com>
2024-05-15 18:05:25 +0200

    Remove LuckPerms and MiniMessage dependencies
    The changes involve the removal of LuckPerms and MiniMessage from the LPC class. This included the deletion of initializations in the constructor and the onEnable method.

--- src/main/java/de/ayont/lpc/LPC.java
+++ src/main/java/de/ayont/lpc/LPC.java
@@ -2,25 +2,15 @@ package de.ayont.lpc;
 
 import de.ayont.lpc.commands.LPCCommand;
 import de.ayont.lpc.listener.AsyncChatListener;
-import net.kyori.adventure.text.minimessage.MiniMessage;
-import net.luckperms.api.LuckPerms;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
 public final class LPC extends JavaPlugin {
 
-    private LuckPerms luckPerms;
-    private final MiniMessage miniMessage;
-
-    public LPC() {
-        this.miniMessage = MiniMessage.builder().build();
-    }
 
     @Override
     public void onEnable() {
-        this.luckPerms = getServer().getServicesManager().load(LuckPerms.class);
         registerCommand();
-
         saveDefaultConfig();
         registerListeners();
     }
