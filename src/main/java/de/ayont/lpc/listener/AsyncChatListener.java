fc3cb7ea1437ab7101265049ae7d02da22676b61
Ayont <ayontprivat@gmail.com>
2024-05-15 18:04:59 +0200

    Update AsyncChatListener and add Utils class
    Updated the functions in the AsyncChatListener class to enhance chat event handling. Incorporated rendering source for events connected to player's inventory. Added a new Utils class for handling text serialization and content checks within chat components.

--- src/main/java/de/ayont/lpc/listener/AsyncChatListener.java
+++ src/main/java/de/ayont/lpc/listener/AsyncChatListener.java
@@ -2,20 +2,41 @@ package de.ayont.lpc.listener;
 
 import de.ayont.lpc.LPC;
 import de.ayont.lpc.renderer.LPCChatRenderer;
+import de.ayont.lpc.utils.Utils;
 import io.papermc.paper.event.player.AsyncChatEvent;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextReplacementConfig;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+
+import static java.util.regex.Pattern.*;
 
 public class AsyncChatListener implements Listener {
 
     private final LPC plugin;
+    private final LPCChatRenderer lpcChatRenderer;
 
     public AsyncChatListener(LPC plugin) {
         this.plugin = plugin;
+        this.lpcChatRenderer = new LPCChatRenderer(plugin);
     }
 
     @EventHandler
     public void onChat(AsyncChatEvent event) {
+
+        final Player player = event.getPlayer();
+        final ItemStack item = player.getInventory().getItemInMainHand();
+        final Component displayName = item.getItemMeta() != null && item.getItemMeta().hasDisplayName() ? item.getItemMeta().displayName() : Component.text(item.getType().toString().toLowerCase().replace("_", " "));
+        if (Utils.contains(event.message(), "[item]")) {
+            item.getType();
+        }
+
         event.renderer(new LPCChatRenderer(plugin));
+
+        event.renderer((source, sourceDisplayName, message, viewer) -> lpcChatRenderer.render(source, sourceDisplayName, message, viewer)
+                .replaceText(TextReplacementConfig.builder().match(compile("\\[item]", CASE_INSENSITIVE))
+                        .replacement(displayName).build()).hoverEvent(item));
     }
 }
