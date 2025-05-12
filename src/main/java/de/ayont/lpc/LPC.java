aa835890785cb744d036f265d41ac4812023a660
Ayont <ayontprivat@gmail.com>
2025-05-12 02:32:21 +0200

    feat: add support for Paper and Spigot APIs with conditional listener registration

--- src/main/java/de/ayont/lpc/LPC.java
+++ src/main/java/de/ayont/lpc/LPC.java
@@ -2,14 +2,27 @@ package de.ayont.lpc;
 
 import de.ayont.lpc.commands.LPCCommand;
 import de.ayont.lpc.listener.AsyncChatListener;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import de.ayont.lpc.listener.SpigotChatListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
 public final class LPC extends JavaPlugin {
+    private boolean isPaper;
 
+    private static final LegacyComponentSerializer legacySerializer = LegacyComponentSerializer.builder()
+            .character('§')
+            .hexColors()
+            .useUnusualXRepeatedCharacterHexFormat()
+            .build();
+
+    public static LegacyComponentSerializer getLegacySerializer() {
+        return legacySerializer;
+    }
 
     @Override
     public void onEnable() {
+        this.isPaper = checkIfPaper();
         registerCommand();
         saveDefaultConfig();
         registerListeners();
@@ -23,8 +36,23 @@ public final class LPC extends JavaPlugin {
         this.getCommand(commandName).setTabCompleter(lpcCommand);
     }
 
-    public void registerListeners() {
-        getServer().getPluginManager().registerEvents(new AsyncChatListener(this), this);
+    private boolean checkIfPaper() {
+        try {
+            Class.forName("io.papermc.paper.event.player.AsyncChatEvent");
+            getLogger().info("Paper API has been detected and will be used.");
+            return true;
+        } catch (ClassNotFoundException e) {
+            getLogger().info("Spigot API has been detected and will be used.");
+            return false;
+        }
+    }
+
+    private void registerListeners() {
+        if (isPaper) {
+            getServer().getPluginManager().registerEvents(new AsyncChatListener(this), this);
+        } else {
+            getServer().getPluginManager().registerEvents(new SpigotChatListener(this), this);
+        }
     }
 
 }
--- src/main/java/de/ayont/lpc/commands/LPCCommand.java
+++ src/main/java/de/ayont/lpc/commands/LPCCommand.java
@@ -1,6 +1,12 @@
 package de.ayont.lpc.commands;
 
 import de.ayont.lpc.LPC;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import de.ayont.lpc.LPC;
 import net.kyori.adventure.text.Component;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -25,18 +31,18 @@ public class LPCCommand implements CommandExecutor, TabCompleter {
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
         if (args.length == 1 && "reload".equals(args[0])) {
             plugin.reloadConfig();
+            String rawReloadMessage = plugin.getConfig().getString("reload-message", "<green>Reloaded LPC Configuration!</green>");
+            Component message = MiniMessage.miniMessage().deserialize(rawReloadMessage);
 
-            String rawReloadMessage = plugin.getConfig().getString("reload-message", "<green>Reloaded LPC Configuration!");
-            Component reloadMessage;
-            reloadMessage = MiniMessage.miniMessage().deserialize(rawReloadMessage);
-
-            sender.sendMessage(reloadMessage);
+            if (plugin.getServer().getName().toLowerCase().contains("paper")) {
+                sender.sendMessage(message);
+            } else {
+                sender.sendMessage(LPC.getLegacySerializer().serialize(message));
+            }
             return true;
         }
-
         return false;
     }
-
 
 
     public List<String> onTabComplete(final @NotNull CommandSender sender, final @NotNull Command command, final @NotNull String alias, final String[] args) {
